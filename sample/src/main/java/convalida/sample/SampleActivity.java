e867ea81090d0cef3d259b98adb7715b3ba35bbc
WellingtonCosta <wellington.costa128@gmail.com>
2017-06-20 15:35:49 -0300

    Example

--- sample/src/main/java/convalida/sample/SampleActivity.java
+++ sample/src/main/java/convalida/sample/SampleActivity.java
@@ -1,6 +1,5 @@
 package convalida.sample;
 
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,38 +9,49 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import convalida.annotations.NotEmptyValidation;
-import convalida.library.Convalida;
 
 public class SampleActivity extends AppCompatActivity {
 
-    @BindView(R.id.activity_sample)
+    @BindView(R.id.linear_layout)
     LinearLayout linearLayout;
 
     @NotEmptyValidation
     @BindView(R.id.name_layout)
     TextInputLayout nameLayout;
 
+    @NotEmptyValidation
     @BindView(R.id.email_layout)
     TextInputLayout emailLayout;
 
+    @NotEmptyValidation
     @BindView(R.id.phone_layout)
     TextInputLayout phoneLayout;
 
+    @NotEmptyValidation
     @BindView(R.id.password_layout)
     TextInputLayout passwordLayout;
 
+    SampleActivity_Validation validation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
         ButterKnife.bind(this);
-        Convalida.init(this);
+        //Convalida.init(this);
+        validation = new SampleActivity_Validation(this);
     }
 
     @OnClick(R.id.validate_button)
     public void validateFields() {
-        boolean isValid = Convalida.validate();
-        Snackbar.make(linearLayout, "Is valid: " + isValid, Snackbar.LENGTH_LONG).show();
+        validation.validate();
+    }
+
+    @OnClick(R.id.clear_button)
+    public void clearFields() {
+        /*boolean isValid = Convalida.validate();
+        Snackbar.make(linearLayout, "Is valid: " + isValid, Snackbar.LENGTH_LONG).show();*/
+
+        validation.clear();
     }
 }
