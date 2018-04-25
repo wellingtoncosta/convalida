e94ca6d865d036273e75411a9e16b99123115a3e
Wellington Costa <wellington.costa128@gmail.com>
2018-04-25 07:45:21 -0300

    Improved CpfValidator tests

--- convalida-validators/src/test/java/convalida/validators/CpfValidatorTest.java
+++ convalida-validators/src/test/java/convalida/validators/CpfValidatorTest.java
@@ -11,10 +11,17 @@ import static org.mockito.Mockito.when;
 public class CpfValidatorTest extends BaseTest {
 
     // Cpf generated on https://www.4devs.com.br/gerador_de_cpf
+
     @Test public void validCpf() {
         CpfValidator validator = new CpfValidator(mockEditText, errorMessage, true);
         when(mockEditText.getText().toString()).thenReturn("32454401037");
         assertEquals(validator.validate(), true);
+    }
+
+    @Test public void invalidCpfWithValueLessThan11() {
+        CpfValidator validator = new CpfValidator(mockEditText, errorMessage, true);
+        when(mockEditText.getText().toString()).thenReturn("123");
+        assertEquals(validator.validate(), false);
     }
 
     @Test public void invalidCpf() {
